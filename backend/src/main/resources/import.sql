ee83d7af14f35e0f2768d80a269fa5e983a026ef
Nico <nicobuquetcebada@gmail.com>
2025-06-02 12:28:29 +0200

    cambio de contenido multimedia por defecto

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -8,11 +8,11 @@ ALTER TABLE posts DROP CONSTRAINT fk89ik4e9tqc05yvnct49n1fhto;
 
 -- Users
 INSERT INTO users (name, mail, first_name, surname, pass, profile_img, role) VALUES
-('nico', 'nico@gmail.com', 'Nicolas', 'Buquet', '$2a$10$EfZNeGwyxw4xegJYnvamE.kHlUmVeE/32iEgtTUhjqFSRncRuK64y', 'http://localhost:8080/api/media/defaultu.jpg', 'user'),
-('jorge', 'jorge@gmail.com', 'Jorge', 'Lopez', '$2a$10$EfZNeGwyxw4xegJYnvamE.kHlUmVeE/32iEgtTUhjqFSRncRuK64y', 'http://localhost:8080/api/media/defaultu.jpg', 'user'),
-('johndoe', 'john@example.com', 'john', 'doe', 'pass1', 'http://localhost:8080/api/media/defaultu.jpg', 'user'),
-('janedoe', 'jane@example.com', 'jane', 'doe',  'pass2', 'http://localhost:8080/api/media/defaultu.jpg', 'user'),
-('bobsmith', 'bob@example.com', 'bob', 'smith',  'pass3', 'http://localhost:8080/api/media/defaultu.jpg', 'admin');
+('nico', 'nico@gmail.com', 'Nicolas', 'Buquet', '$2a$10$EfZNeGwyxw4xegJYnvamE.kHlUmVeE/32iEgtTUhjqFSRncRuK64y', 'http://localhost:8080/api/media/defaultu.png', 'user'),
+('jorge', 'jorge@gmail.com', 'Jorge', 'Lopez', '$2a$10$EfZNeGwyxw4xegJYnvamE.kHlUmVeE/32iEgtTUhjqFSRncRuK64y', 'http://localhost:8080/api/media/defaultu.png', 'user'),
+('johndoe', 'john@example.com', 'john', 'doe', 'pass1', 'http://localhost:8080/api/media/defaultu.png', 'user'),
+('janedoe', 'jane@example.com', 'jane', 'doe',  'pass2', 'http://localhost:8080/api/media/defaultu.png', 'user'),
+('bobsmith', 'bob@example.com', 'bob', 'smith',  'pass3', 'http://localhost:8080/api/media/defaultu.png', 'admin');
 
 -- Albums
 INSERT INTO albums (id, name, cover_img, date, user_name) VALUES
@@ -69,7 +69,7 @@ INSERT INTO songs (id, name, cover_img, date, audio, user_name, album_id) VALUES
 (1, 'Sunshine', 'http://localhost:8080/api/media/defaultc.png', '2023-06-15', 'http://localhost:8080/api/media/default.mp3', 'bobsmith', 1),
 (2, 'Snowfall', 'http://localhost:8080/api/media/defaultc.png', '2023-12-15', 'http://localhost:8080/api/media/default.mp3', 'bobsmith', 1),
 (3, 'Bloom', 'http://localhost:8080/api/media/defaultc.png', '2023-03-15', 'http://localhost:8080/api/media/default.mp3', 'janedoe', null),
-(4, 'mastercaster', 'http://localhost:8080/api/media/defaultc.png', '2023-03-15', 'http://localhost:8080/api/media/default.mp3', 'nico', null);
+(4, 'Reputation', 'http://localhost:8080/api/media/gif-test.gif', '2023-03-15', 'http://localhost:8080/api/media/default.mp3', 'nico', null);
 ALTER SEQUENCE songs_seq RESTART WITH 5;
 
 DROP TABLE IF EXISTS search_view CASCADE;
